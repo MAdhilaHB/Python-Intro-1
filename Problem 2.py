70546edbf41428f20eec2ca653a03325961cfd5f
MAdhilaHB <60499188+MAdhilaHB@users.noreply.github.com>
2020-02-08 15:07:28 +0530

    Add files via upload

--- Problem 2.py
+++ Problem 2.py
@@ -1,6 +1,10 @@
 
-a = 'HumptyDumptysatonawallHumptyDumptyhadagreatfallAlltheKingshorsesandalltheKingsmenCouldntputHumptyDumptyinhisplaceagain'
-b = '22' , '27', '97' , '102',
+s = "HumptyDumptysatonawallHumptyDumptyhadagreatfallAlltheKingshorsesandalltheKingsmenCouldntputHumptyDumptyinhisplaceagain"
 
-print ( a[0:6] + ' ' + a[6:12] )
 
+a = 22
+b = 27
+c = 97
+d = 102
+
+print ( s[ a : b+1 ] + " " + s[ c : d +1 ] )
